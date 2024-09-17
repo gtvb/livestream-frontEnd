9554561f57e278976f65d4337dac76bb91d7e010
ggemelle <gabriel.spgemelli@gmail.com>
2024-09-17 09:42:24 -0300

    :recycle: recriando a tela de login

--- livestream/src/app/login/index.tsx
+++ livestream/src/app/login/index.tsx
@@ -1,53 +1,21 @@
-"use client";
-
-import React, { useState } from "react";
 import styles from './index.module.css';
 
-const Page: React.FC = () => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
 
-  const handleSubmit = (event: React.FormEvent) => {
-    event.preventDefault();
-    // Lógica de autenticação aqui
-    console.log({ email, password });
-  };
-
-  return (
-    <div className={styles.page1}>
-      <div className={styles.telalogin}>
-        <form onSubmit={handleSubmit}>
-          <div className={styles.username}>
-            <label>USERNAME:</label>
-          </div>
-          <div className={styles.usernamerec}>
-            <input
-              type="text"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className={styles.password}>
-            <p className={styles.password1}>PASSWORD</p>
-          </div>
-          <div className={styles.passwordrec}>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className={styles.login}>
-            LOGIN
-          </button>
-        </form>
-      </div>
-      <div className={styles.createUser}>CREATE USER</div>
-      <img className={styles.selolivestreamIcon} alt="" src="./seloLivestream.png" />
-    </div>
-  );
+const Page = () => {
+	return (
+		<div className={styles.page1}>
+			<div className={styles.telalogin} />
+			<div className={styles.passwordrec} />
+			<div className={styles.entrarEmLivestream}>Entrar em Livestream</div>
+			<div className={styles.senha}>Senha</div>
+			<div className={styles.passwordrec1} />
+			<div className={styles.usernamerec} />
+			<div className={styles.aindaNoPossui}>Ainda não possui uma conta? Cadastrar-se</div>
+			<div className={styles.problemasParaEfetuar}>Problemas para efetuar login?</div>
+			<div className={styles.usurio}>Usuário</div>
+			<div className={styles.entrar}>Entrar</div>
+			<img className={styles.fire1Icon} alt="" src="fire 1.svg" />
+		</div>);
 };
 
 export default Page;
