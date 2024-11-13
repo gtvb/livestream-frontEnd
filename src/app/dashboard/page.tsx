5288612be3e393ea98d8d80f8eb4d2315297b7a2
diegojrodriguess <diego.rodrigues@ges.inatel.br>
2024-11-13 00:15:18 -0300

    começando a levar a estilização da pagina dashboard para o estilo do design

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -10,8 +10,19 @@ export default async function Dashboard() {
 
     return (
         <div className={styles.dashboard}>
+            <img className={styles.blurIcon} alt="Blur" src="/assets/images/blur.png" />
             <div className={styles.header}>
                 <h2 className={styles.title}>Bem-vindo ao seu Dashboard</h2>
+                <form action={async () => {
+                    "use server"
+                    await signOut({ redirectTo: "/live/create" });
+                }}>
+                    <button className={styles.createLiveButton}>
+                    <img  className={"styles.videoCamera"} alt="" src="/assets/images/VideoCamera.png" />
+                        Criar Live
+                    </button>
+                </form>
+
                 <form action={async () => {
                     "use server"
                     await signOut({ redirectTo: "/login" });
@@ -21,7 +32,7 @@ export default async function Dashboard() {
             </div>
 
             <div className={styles.content}>
-                <Link href={"/profile"} className={styles.link}>
+                <Link href={"/profile"} className={styles.profileButton}>
                     Acessar Perfil
                 </Link>
 
