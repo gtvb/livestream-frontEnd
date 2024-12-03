acf5137158de91f4e612e1dad05c386cb6cab977
gtvb <gabriel.tiso@proton.me>
2024-12-03 14:03:57 -0300

    testando .env

--- src/app/ui/slider.tsx
+++ src/app/ui/slider.tsx
@@ -18,6 +18,8 @@ export default function LivestreamNavigator({ feed }: { feed: Feed }) {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + feed.livestreams.length) % feed.livestreams.length);
     };
 
+    console.log(process.env.NEXT_PULIC_API_URL);
+
     return (
         <div className={styles.navigatorContainer}>
             {feed.livestreams.length == 0 ? (
